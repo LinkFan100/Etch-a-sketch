9a481989840ca65edcfff3a877525d9fd166f7a3
Lafyette Russell <121630484+LinkFan100@users.noreply.github.com>
2025-02-27 11:12:23 -0600

    Add mainDiv in index, add div creation code in etch.js, add styles to mainDiv and divs created

---
+++ etch.js
@@ -0,0 +1,18 @@
+
+function divCreate(divsNum,mainDivClass){
+    const mainDiv = document.querySelector(mainDivClass);
+    if(!mainDiv){
+        console.error("No such class exist");
+        return;
+    }
+    for(let i = 0;i<divsNum;i++){
+        const divs = document.createElement("div");
+        divs.style.border = '1px solid red';
+        divs.style.padding = '30px';
+        // divs.style.margin = '5px';
+        divs.classList.add("gridItem");
+        mainDiv.appendChild(divs);
+    }
+    
+}
+divCreate(16,".mainDiv")
