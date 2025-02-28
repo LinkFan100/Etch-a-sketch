40a6b90dca2cf27905b4e9a163a6fda2f74e6a28
Lafyette Russell <121630484+LinkFan100@users.noreply.github.com>
2025-02-28 10:58:25 -0600

    Remove hover effect from css,add effect via javascript

--- etch.js
+++ etch.js
@@ -9,10 +9,19 @@ function divCreate(divsNum,mainDivClass){
         const divs = document.createElement("div");
         divs.style.border = '1px solid red';
         divs.style.padding = '30px';
-        // divs.style.margin = '5px';
         divs.classList.add("gridItem");
         mainDiv.appendChild(divs);
     }
     
 }
-divCreate(16,".mainDiv")
+divCreate(16,".mainDiv");
+const gridItem = document.querySelectorAll(".gridItem");
+gridItem.forEach(i => {
+    i.addEventListener("mousemove",function(event){
+        i.style.backgroundColor = "blue";
+    })
+    i.addEventListener("mouseout",function(event){
+        i.style.backgroundColor = "white";
+    })
+    
+});
